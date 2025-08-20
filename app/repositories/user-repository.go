8e56e5520deceb0c53007f94dc91f61464fb5aa5
reganputra <ramadhanregan334@gmail.com>
2025-08-21 06:53:47 +0700

    feat: add get user name and create user session repository

--- app/repositories/user-repository.go
+++ app/repositories/user-repository.go
@@ -9,3 +9,12 @@ import (
 func CreateUser(ctx context.Context, user *models.User) error {
 	return database.DB.Create(user).Error
 }
+
+func GetUserByUsername(ctx context.Context, username string) (models.User, error) {
+	var user models.User
+	return user, database.DB.Where("username = ?", username).Last(&user).Error
+}
+
+func CreateUserSession(ctx context.Context, session *models.UserSession) error {
+	return database.DB.Create(session).Error
+}
