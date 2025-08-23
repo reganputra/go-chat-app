e81f104cd2dad11f9da463bffb7be0ec57aad4f7
reganputra <ramadhanregan334@gmail.com>
2025-08-23 15:51:58 +0700

    feat: add logging functionality and configure ELK stack for chat message logging

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -5,6 +5,9 @@ import (
 	"go-chat-app/pkg/database"
 	"go-chat-app/pkg/env"
 	"go-chat-app/pkg/router"
+	"io"
+	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -15,14 +18,28 @@ import (
 
 func NewApplication() *fiber.App {
 	env.SetupEnvFile()
+	SetupLogFile()
+
 	database.SetupDatabase()
 	database.SetupMongoDb()
+
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{Views: engine})
 	app.Use(recover.New())
 	app.Use(logger.New())
 	app.Get("/dashboard", monitor.New())
+
 	go websocket.ServeWsMessage(app)
+
 	router.InstallRouter(app)
 	return app
+}
+
+func SetupLogFile() {
+	logFile, err := os.OpenFile("./logs/chat_message.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
 }
