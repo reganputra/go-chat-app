8f74f02efe799cfe879cb02a50d04f4d28d146cd
reganputra <ramadhanregan334@gmail.com>
2025-08-25 17:40:52 +0700

    refactor: replace godotenv with os.LookupEnv for environment variable retrieval

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -1,22 +1,16 @@
 package env
 
-import "github.com/joho/godotenv"
+import "os"
 
 var Env map[string]string
 
 func GetEnv(key, def string) string {
-	if val, ok := Env[key]; ok {
+	if val, ok := os.LookupEnv(key); ok {
 		return val
 	}
 	return def
 }
 
 func SetupEnvFile() {
-	envFile := ".env"
-	var err error
-	Env, err = godotenv.Read(envFile)
-	if err != nil {
-		panic(err)
-	}
 
 }
